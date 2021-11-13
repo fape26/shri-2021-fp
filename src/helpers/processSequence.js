6e58b996a094adc7625737b30126478290b8bfa2
FAPE <26fape@gmail.com>
2021-11-13 23:40:39 +0300

    done second part of the task

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -21,12 +21,16 @@ import Api from '../tools/api';
 
 
 const api = new Api();
+const getAnimalById = (id) => api.get(`https://animals.tech/${id}`)({});
+const getTech = api.get('https://api.tech/numbers/base');
 
+const getBinary = (input) => getTech({from: 10, to: 2, number: input});
 
 const getValue = prop('value'),
     getWriteLog = prop('writeLog'),
     getSuccess = prop('handleSuccess'),
-    getError = prop('handleError');
+    getError = prop('handleError'),
+    getResult = prop('result');
 
 
 const isNumber = test(/^\d+(\.\d+)?$/),
@@ -47,7 +51,9 @@ const isNotValidatedValue = compose(
     isValidatedValue
 );
 
-
+const parseNumber = compose(Math.round, parseFloat, getValue),
+    divideThree = (input) => input % 3,
+    powNumber = (input) => Math.pow(input, 2);
 
 const logError = (input) => getError(input)('ValidationError')
 
@@ -62,6 +68,19 @@ const processSequence = (input) => {
             isValidatedValue,
             compose(
                 andThen(handleSuccess),
+                andThen(getResult),
+                andThen(getAnimalById),
+                andThen(tap(writeLog)),
+                andThen(divideThree),
+                andThen(tap(writeLog)),
+                andThen(powNumber),
+                andThen(tap(writeLog)),
+                andThen(length),
+                andThen(tap(writeLog)),
+                andThen(getResult),
+                getBinary,
+                tap(writeLog),
+                parseNumber
             )
         ),
         when(
